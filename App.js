9235fd52c74ca87257b9eb12d2bb44b87917a459
Leo <larroyos1@toluca.tecnm.mx>
2021-10-23 19:44:21 -0500

    Actualizacion

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import "react-native-gesture-handler";
 import React from 'react';
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 //Contenedor de Navegador
