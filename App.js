10ff8a4a63470c55eb9b0e517f5cb4acd9eefdb7
Leo <larroyos1@toluca.tecnm.mx>
2021-11-07 18:15:57 -0600

    Correccion

--- App.js
+++ App.js
@@ -3,34 +3,36 @@ import React, { setState, useEffect, useState } from 'react';
 import { View, Text } from "react-native-animatable";
 import { NavigationContainer } from "@react-navigation/native";
 import { useNetInfo } from "@react-native-community/netinfo";
-import * as Notifications from 'expo-notifications';
+//import * as Notifications from 'expo-notifications';
 
 //Contenedor de Navegador
 import NavegacionSolecc from "./app/navegacion/navegacionSolecc";
+
+/*
+const getToken = async () => {
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    alert('Failed to get push token for push notification!');
+    return;
+  }
+  const token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log(token);
+
+  return token;
+}
+*/
 
 const App = () => {
 
   const internet = useNetInfo().isInternetReachable;
 
   useEffect(() => {
-    /*
-    const VerNotificaciones = async() => {  
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-      const token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-    }
-
-    VerNotificaciones();
-    */
+    //getToken();
   }, []);
 
   return (
